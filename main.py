80544e900293dad1a81aced936071e5f78a44fee
Kode-n-Rolla <111082113+Kode-n-Rolla@users.noreply.github.com>
2022-10-13 17:49:56 +0200

    Add english

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 from card import *
 from player import *
 
-"""Главная игровая функция"""
+"""Main funcion"""
 def black_jack():
     this_deck = Deck()
-    player = Player('Френки')
-    dealer = Player('Дилер')
+    player = Player('Frencky')
+    dealer = Player('Dealer')
     player.add_card(this_deck.deal_card())
     player.add_card(this_deck.deal_card())
     dealer.add_card(this_deck.deal_card())
-    print('Карты дилера: ', dealer)
-    print(f'Карты {player.name}: ', player)
+    print('Dealer`s cards: ', dealer)
+    print(f'{player.name}`s cards: ', player)
 
     status_game = True
 
     while player.get_value() < 21:
-        choice = input('Взять карту или нет? (y/n): ').lower()
+        choice = input('Take card or not? (y/n): ').lower()
         if choice == 'y':
             player.add_card(this_deck.deal_card())
-            print(f'Карты {player.name}: ', player)
+            print(f'{player.name}`s cards: ', player)
             if player.get_value() > 21:
-                print(f'{player.name} проиграл')
+                print(f'{player.name} lose')
                 status_game = False
         else:
-            print(f'{player.name} не берет карты')
+            print(f'{player.name} don`t take card')
             break
 
     if status_game:
         while dealer.get_value() < 17:
             dealer.add_card(this_deck.deal_card())
-            print('Карты дилера: ', dealer)
+            print('Dealer`s cards: ', dealer)
             if dealer.get_value() > 21:
-                print('Дилер проиграл')
+                print('Dealer lose')
                 status_game = False
     if status_game:
         if player.get_value() > dealer.get_value():
-            print(f'{player.name} выиграл')
+            print(f'{player.name} win')
         else:
-            print('Дилер выиграл')
+            print('Dealer win')
 
 black_jack()
