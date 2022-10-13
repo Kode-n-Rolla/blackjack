f6ac64e0a73cc92a30e48a7361e51cdbc9f1730e
Kode-n-Rolla <111082113+Kode-n-Rolla@users.noreply.github.com>
2022-10-13 17:47:17 +0200

    Add english language

--- player/__init__.py
+++ player/__init__.py
@@ -1,15 +1,15 @@
 class Player(object):
     
-    """Инициализия класса по имени и карт в руке"""
+    """Initialized class with name and hands cards"""
     def __init__(self, name) -> None:
         self.name = name
         self.cards = []
         
-    """Добавление карт в руку"""
+    """Add card to hands"""
     def add_card(self, card) -> None:
         self.cards.append(card)
 
-    """Получение очков достоинства карт в рукt"""
+    """Get points equal card value"""
     def get_value(self) -> int:
         result = 0
         aces = 0
@@ -21,10 +21,10 @@ class Player(object):
             result += aces * 10
         return result
 
-    """Вывод достоинства карт в руке"""
+    """Outpu card value"""
     def __str__(self) -> str:
         text = ''
         for card in self.cards:
             text += str(card) + ' '
-        text += 'Значения на руке: ' + str(self.get_value())
+        text += 'Cards value: ' + str(self.get_value())
         return text
